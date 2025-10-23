1670f0f62de2fa58c75ba9d3f0cad5032b5c7f89
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-23 08:04:39 +0700

    update main

--- QLSV/main.py
+++ QLSV/main.py
@@ -4,6 +4,25 @@ Minh họa OOP
 
 # Định nghĩa lớp sinh viên
 class Student:
-    def __init__(self, name, age):
+    # Thuộc tính tĩnh chứa tổng số SV được tạo ra
+    totalCount = 0
+    # Phương thức khởi tạo
+    def __init__(self, id, name, grade):
+        self.id = id
         self.name = name
-        self.age = age
+        self.grade = grade
+    # Pương thức in thông tin SV
+    def DisplayInfo(self):
+        print("Student ID:", self.id)
+        print("Student Name:", self.name)
+        print("Student Grade:", self.grade)
+    # Nạp chồng phương thức
+    def ToString(self):
+        return (f"{self.id}\t{self.name}\t{self.grade}")
+
+# Chương trình chính
+if __name__ == 'main':
+    # Khởi tạo đô tượng sinh viên
+    student = Student("65133958", "trongdepzai", 8.56)
+    # student.DisplayInfo()
+    print(student.ToString())
